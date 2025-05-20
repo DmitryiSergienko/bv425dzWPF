a8822e1ea034e0c3172dd33253d7b1bdb3686a22
Dimka <godslim1992@gmail.com>
2025-05-20 06:20:10 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+
+namespace bv425dzWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SumNumbers(object sender, RoutedEventArgs e)
+    {
+        double xValue;
+        double yValue;
+        if (string.IsNullOrEmpty(firstValue.Text) || string.IsNullOrEmpty(secondValue.Text))
+        {
+            disPlay.Text = "Заполните поля!";
+        }
+        else if (double.TryParse(firstValue.Text, out xValue) && double.TryParse(secondValue.Text, out yValue))
+        { 
+            disPlay.Text = $"{xValue} + {yValue} = {xValue + yValue}";
+        }
+        else
+        {
+            disPlay.Text = "Не корректный ввод!";
+        }
+        disPlay.ToolTip = disPlay.Text;
+    }
+}
